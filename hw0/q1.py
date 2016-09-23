4e993c3ef0eeacdbc707a49fd6139ec014dd1045
NTUwanderer <harveyycyang@gmail.com>
2016-09-23 16:48:10 +0800

    print removed

--- hw0/q1.py
+++ hw0/q1.py
@@ -1,10 +1,8 @@
 import sys, copy
-import numpy as np
 
 def getValue(item):
 	return float(item)
 
-# print(sys.argv)
 index = int(sys.argv[1])
 filename = sys.argv[2]
 with open(filename) as inf:
@@ -17,7 +15,6 @@ numbers = []
 for element in data:
 	numbers.append(element[index])
 numbers = sorted(numbers, key=getValue)
-print(numbers)
 last = numbers.pop()
 file = open('ans1.txt', 'w')
 for element in numbers:
